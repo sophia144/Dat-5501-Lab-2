2b9a5b0bf1d6b7219be470d53cc976980711b2ab
sophia144 <piercesp@outlook.com>
2025-09-25 15:09:54 +0100

    Imported unit test and began to write the testing class.

--- unit_testing.py
+++ unit_testing.py
@@ -1,2 +1,12 @@
+import unittest
+
 def subtract_numbers(num1, num2):
     return num1 - num2
+
+class subtraction_checks:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def subtract(self):
+        return self.num1 - self.num2
