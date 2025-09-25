dce3b50a3f84a931e35d45f2053d19d2b4932e96
sophia144 <piercesp@outlook.com>
2025-09-25 15:27:57 +0100

    Added more unit tests.

--- unit_testing.py
+++ unit_testing.py
@@ -5,8 +5,17 @@ def subtract_numbers(num1, num2):
 
 class subtraction_checks(unittest.TestCase):
     def test_unit(self):
+
         function_output = subtract_numbers(8, 2)
         expected_output = 6
+        self.assertEqual(function_output, expected_output, f"Fail: expected {expected_output}, got {function_output}")
+
+        function_output = subtract_numbers(-24, 5)
+        expected_output = -29
+        self.assertEqual(function_output, expected_output, f"Fail: expected {expected_output}, got {function_output}")
+
+        function_output = subtract_numbers(-6, -7)
+        expected_output = 1
         self.assertEqual(function_output, expected_output, f"Fail: expected {expected_output}, got {function_output}")
 
 
