cfdeb25a38fcb9598258f35e34d025aa4c86fbe5
sophia144 <piercesp@outlook.com>
2025-09-25 15:22:38 +0100

    Completed the unit test for subtraction function.

--- unit_testing.py
+++ unit_testing.py
@@ -3,10 +3,12 @@ import unittest
 def subtract_numbers(num1, num2):
     return num1 - num2
 
-class subtraction_checks:
-    def __init__(self, num1, num2):
-        self.num1 = num1
-        self.num2 = num2
+class subtraction_checks(unittest.TestCase):
+    def test_unit(self):
+        function_output = subtract_numbers(8, 2)
+        expected_output = 6
+        self.assertEqual(function_output, expected_output, f"Fail: expected {expected_output}, got {function_output}")
 
-    def subtract(self):
-        return self.num1 - self.num2
+
+if __name__ == '__main__':
+    unittest.main()
